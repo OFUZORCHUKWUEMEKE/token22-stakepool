91b83679fedaddf5247157c21e8d85d1373ed354
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2024-09-22 10:49:01 +0000

    initialized

---
+++ programs/token22/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("2M8W4edWQnie9q3HKachzwXkd1FrrCA4aC3JUdNn6WYY");
+
+#[program]
+pub mod token22 {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
