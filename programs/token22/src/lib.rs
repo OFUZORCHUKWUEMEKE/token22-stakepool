f0d0231d8abbe460a1584dc4b726682c64023d50
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2024-09-24 03:04:40 +0000

    up-to-date

--- programs/token22/src/lib.rs
+++ programs/token22/src/lib.rs
@@ -9,12 +9,20 @@ declare_id!("2M8W4edWQnie9q3HKachzwXkd1FrrCA4aC3JUdNn6WYY");
 #[program]
 pub mod token22 {
     use super::*;
+    
+    pub fn init_pool(ctx: Context<InitializePool>) -> Result<()> {
+        init_pool::handler(ctx)
+    }
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn init_stake_entry(ctx: Context<InitializeStakeEntry>) -> Result<()> {
+        init_stake_entry::handler(ctx)
+    }
+    
+    pub fn stake(ctx: Context<Stake>, amount: u64) -> Result<()> {
+        stake::handler(ctx, amount)
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        unstake::handler(ctx)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
